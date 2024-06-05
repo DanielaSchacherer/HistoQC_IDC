25d356f724862e4613bb34e5230872d1af64b19f
ds_93 <daniela.schacherer@web.de>
2024-06-05 18:25:40 +0200

    dev

--- histoqc/SaveModule.py
+++ histoqc/SaveModule.py
@@ -26,7 +26,7 @@ def saveFinalMask(s, params):
     logging.info(f"{s['filename']} - \tsaveUsableRegion")
 
     mask = s["img_mask_use"]
-    message = f"{img_as_ubyte(mask).max()}, {img_as_ubyte(mask).min()}"
+    message = f"{np.unique(img_as_ubyte(mask))}"
     logging.warning(message)
     s["warnings"].append(message)
     for mask_force in s["img_mask_force"]:
